8c0903f14052b340cb2f7d644d4a065bc956d1ec
migafgarcia <migafgarcia@gmail.com>
2016-12-01 13:22:35 +0000

    Added ice storm project for reference
    Incomplete

---
+++ ice_storm_test/src/main/java/server/PrinterI.java
@@ -0,0 +1,14 @@
+package server;
+
+import Demo._PrinterDisp;
+import Ice.Current;
+
+/**
+ * Created by mgarcia on 29-11-2016.
+ */
+public class PrinterI extends _PrinterDisp {
+    @Override
+    public void printString(String s, Current __current) {
+        System.out.println(s);
+    }
+}
